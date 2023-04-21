395c1087fad8d80053a20f613f912dcb7292e326
Steven3020 <130913199+MagicLyani@users.noreply.github.com>
2023-04-21 02:28:33 +0200

    Resolution check

---
+++ index.js
@@ -0,0 +1,6 @@
+function New_Wiki() {
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    document.getElementById("X").innerHTML = w;
+    document.getElementById("Y").innerHTML = h;
+}
